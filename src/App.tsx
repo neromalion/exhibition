df63630f53937a09f6332496e5d19cac2e1e89b4
nero <neromalion@gmail.com>
2022-05-06 22:30:50 +0900

    feat: mui, iconify

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { Icon } from "@iconify/react";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography align="center">
+      <Button variant="outlined">
+        <Icon icon="fa6-solid:face-smile-wink" />
+        Let's start
+      </Button>
+    </Typography>
   );
 }
 
